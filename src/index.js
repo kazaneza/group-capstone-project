757d38c31746ce006785fcdfcb3458e3a71165f2
kazaneza <kazanezagentil@gmail.com>
2023-02-14 13:09:47 +0100

    display items in console

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 import './style.css';
+import { getMovies } from './module/api';
 
-fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', {
-
-    
-method: 'GET',
-headers: {
-    'X-RapidAPI-Key': 'b3d802e9a6msh4f5f22841b791b6p11e7a8jsnbd6b8d439505',
-    'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-}
-})
-
-.then(response => response.json())
-.then(data => {
-    const list = data.d;
-    
+getMovies()
+  .then(list => {
     list.map((item) => {
-        console.log(item)
-    })
-})
-.catch(err =>{
-    console.error(err)
-} );
+        console.log(item);
+    });
+  })
+  .catch(err => {
+    console.error(err);
+  });
