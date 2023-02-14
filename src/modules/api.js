51190225640d98d8136162b12caf566c005296d1
kazaneza <kazanezagentil@gmail.com>
2023-02-14 13:07:37 +0100

    added api

--- src/modules/api.js
+++
@@ -1,9 +0,0 @@
-const apiUrl = "https://api.tvmaze.com/shows/1/seasons";
-
-const getData = async () => {
-  const response = await fetch(apiUrl);
-  const data = await response.json();
-  return data;
-};
-
-export default getData;
