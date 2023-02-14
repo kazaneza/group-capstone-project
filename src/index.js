427e413c6d89912ce2d4c05e143ca38618bb48b6
kazaneza <kazanezagentil@gmail.com>
2023-02-14 19:25:11 +0100

    displayed in the homepage

--- src/index.js
+++ src/index.js
@@ -1,23 +1,10 @@
 import './style.css';
+import getMovies from './modules/api.js';
+import renderShows from './modules/display';
 
-fetch('https://imdb8.p.rapidapi.com/auto-complete?q=game%20of%20thr', {
+const startApp = async () => {
+  const shows = await getMovies();
+  renderShows(shows);
+};
 
-    
-method: 'GET',
-headers: {
-    'X-RapidAPI-Key': 'b3d802e9a6msh4f5f22841b791b6p11e7a8jsnbd6b8d439505',
-    'X-RapidAPI-Host': 'imdb8.p.rapidapi.com'
-}
-})
-
-.then(response => response.json())
-.then(data => {
-    const list = data.d;
-    
-    list.map((item) => {
-        console.log(item)
-    })
-})
-.catch(err =>{
-    console.error(err)
-} );
+startApp();
