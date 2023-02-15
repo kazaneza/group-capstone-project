b1f688d527848e28c9e4d5f2362941e7c54498e4
Mert Kantaroglu <mertkantaroglu@gmail.com>
2023-02-15 12:49:34 +0300

    changes in commit-popup

--- src/modules/commentPopup.js
+++ src/modules/commentPopup.js
@@ -24,13 +24,13 @@ export const showComments = async (
   `;
 };
 
-// export const commentPopup = () => {
-//   commentButton.forEach((item) => {
-//     item.addEventListener('click', () => {
-//       commentPopupContainer.classList.remove('hidden');
-//     })
-//   })
-// }
+export const commentPopup = () => {
+  commentButton.forEach((item) => {
+    item.addEventListener('click', () => {
+      commentPopupContainer.classList.remove('hidden');
+    })
+  })
+}
 
 export const closeFunc = () => {
   closeIcon.addEventListener('click', () => {
@@ -38,56 +38,3 @@ export const closeFunc = () => {
   });
 };
 
-
-//! ------------------------------------------------------------
-// const popupContainer = document.querySelector('.popup-display');
-
-// window.addEventListener('load', async () => {
-//   const shows = await getMovieData();
-//   renderHomePage(shows);
-//   document.addEventListener('click', async (e) => {
-//     const button = e.target.closest('.comments-btn');
-//     const closeButton = e.target.closest('.close-icon');
-
-//     if (button) {
-//       const selectedShow = shows.filter(
-//         (it) => it.id.toString() === button.id.toString(),
-//       )[0];
-//       const {
-//         id, image, name, language, genres, rating, schedule,
-//       } = selectedShow;
-//       showContainer.style.display = 'none';
-//       popupContainer.style.display = 'block';
-//       renderPopup(id, image, name, language, genres, rating, schedule);
-//     }
-
-//     if (closeButton) {
-//       popupContainer.style.display = 'none';
-//       showContainer.style.display = 'grid';
-//       renderHomePage(shows);
-//     }
-//   });
-
-  // getLike();
-//});
-
-// const modal = document.querySelector('.popup-window');
-// const overlay = document.querySelector('.overlay');
-// const openModalBtn1 = document.querySelector('.project-button1');
-
-// // Opening 1st Card //
-// const openModal1 = function () {
-//   overlay.classList.remove('hidden');
-//   modal.style.display = 'flex';
-
-//   // Populating cards with object items //
-//   popupsProjectImage.src = projects[0].image;
-//   popupsProjectTitle.innerHTML = projects[0].name;
-//   popupsProjectDesktop.innerHTML = projects[0].content1;
-//   popupsProjectMobile.innerHTML = projects[0].content2;
-//   tagsUp.innerHTML = projects[0].tags1;
-//   tagsDown.innerHTML = projects[0].tags2;
-//   document.documentElement.scrollTop = 0;
-// };
-
-// openModalBtn1.addEventListener('click', openModal1);
