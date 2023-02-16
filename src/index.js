f5c8832a61875d2afec0fc3c6dcda0da74052b5a
Nezerwa <eligrand2000@gmail.com>
2023-02-16 19:35:04 +0200

    add reservation popup styles

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import displayHomePage from './modules/display';
 
 import fetchMovies from './modules/api';
 import commentPopup from './modules/commentPopup';
-
+import reservationPopup from './modules/reservation';
 const showContainer = document.querySelector('.show-container');
 const popupContainer = document.querySelector('.comment-popup');
+const reservationContainer = document.querySelector('.reservation-model')
 
 window.addEventListener('load', async () => {
   const shows = await fetchMovies();
@@ -14,7 +15,7 @@ window.addEventListener('load', async () => {
   document.addEventListener('click', async (e) => {
     const commentButton = e.target.closest('.comments-btn');
     const closeButton = e.target.closest('.close-icon');
-
+    const reservationButton = e.target.closest('.reservation-btn');
     if (commentButton) {
       const selectedShow = shows.filter(
         (it) => it.id.toString() === commentButton.id.toString(),
@@ -26,6 +27,16 @@ window.addEventListener('load', async () => {
       popupContainer.style.display = 'block';
       commentPopup(id, image, name, language, genres, rating, schedule);
     }
+     if (reservationButton) {
+       const selectedShow = shows.filter(
+         (it) => it.id.toString() === reservationButton.id.toString()
+       )[0];
+       const { id, image, name, language, genres, rating, schedule } =
+         selectedShow;
+       showContainer.style.display = "none";
+       reservationContainer.style.display = "block";
+       reservationPopup(id, image, name, language, genres, rating, schedule);
+     }
 
     if (closeButton) {
       popupContainer.style.display = 'none';
--- src/modules/display.js
+++ src/modules/display.js
@@ -1,34 +1,23 @@
-const popupContainer = document.querySelector('.comment-popup');
+const showContainer = document.querySelector(".show-container");
 
-const commentPopup = async (
-  id, image, name, language, genres, rating, schedule,
-) => {
-  popupContainer.innerHTML = '';
-
-  popupContainer.innerHTML += `
-        <div class="show-${id} show-poster">
-        <img src=${image.original} alt="" class="movie-img" />
-        <div class="close-icon">X</div>
-      </div>
-      <div class="show-info">
-        <h2 class="show-title">${name}</h2>
-        <div class="show-specs">
-          <p>Language: ${language}</p>
-          <p>Genre: ${genres.join(',')}</p>
-          <p>Rating: ${rating.average}</p>
-          <p>Schedule: ${schedule.time} on ${schedule.days}s</p>
+const displayHomePage = async (shows) => {
+  showContainer.innerHTML = "";
+  shows.forEach((item) => {
+    showContainer.innerHTML += `
+        <div class="show-${item.id} show-poster">
+        <img src=${item.image.medium} alt="movie-image" class="image-pic" />
+        <div class="desc">
+          <p class="movie-title">${item.name}</p>
+          <div class="likes">
+            <i class="fa-sharp fa-regular fa-heart"></i>
+            <p data-id = "${item.id}">5 likes</p>
+          </div>
         </div>
+        <button class="comments-btn" id=${item.id}>Comments</button>
+        <button class="reservation-btn" id=${item.id}>Reservation</button>
       </div>
-
-      <div class="add-comment">
-        <h2>Add Comments</h2>
-        <form>
-          <input type="text" name="text-name" id="text-name" placeholder="Your Name">
-          <textarea name="text-insights" id="text-insights" cols="35" rows="7" placeholder="Your insights"></textarea>
-          <button type="submit">Comment</button>
-        </form>
-      </div>         
     `;
+  });
 };
 
-export default commentPopup;
+export default displayHomePage;
